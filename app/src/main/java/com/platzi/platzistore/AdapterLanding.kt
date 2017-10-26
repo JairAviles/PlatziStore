2565bb13e10ec80bf994e347613afa12fec68ea8
Jair Avilés <hi@jairaviles.mx>
2017-10-25 23:57:52 -0200

    add Transitions view elements to detail activity

--- app/src/main/java/com/platzi/platzistore/AdapterLanding.kt
+++ app/src/main/java/com/platzi/platzistore/AdapterLanding.kt
@@ -1,5 +1,9 @@
 package com.platzi.platzistore
 
+import android.app.Activity
+import android.content.Intent
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.util.Pair
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
@@ -23,8 +27,23 @@ class AdapterLanding(val data: List<ItemLanding>) : RecyclerView.Adapter<Adapter
         fun bindView(itemLanding: ItemLanding) {
             with(itemLanding) {
                 itemView.text_title_item.text = title
-                itemView.text_dec_item.text = desc
+                itemView.text_desc_item.text = desc
                 itemView.text_price_item.text = "$ ${String.format("%.2f", price)}"
+
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailActivity::class.java)
+                    intent.putExtra("title", title)
+                    intent.putExtra("desc", desc)
+                    intent.putExtra("price", price)
+
+                    val p1:Pair<View, String> = Pair.create(itemView.image_item_header, "transitionHeader")
+                    val p2:Pair<View, String> = Pair.create(itemView.text_title_item, "transitionTitle")
+                    val p3:Pair<View, String> = Pair.create(itemView.text_desc_item, "transitionDesc")
+                    val p4:Pair<View, String> = Pair.create(itemView.text_price_item, "transitionPrice")
+                    val options = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity, p1, p2, p3, p4)
+                    itemView.context.startActivity(intent, options.toBundle())
+
+                }
             }
         }
     }
--- app/src/main/java/com/platzi/platzistore/DetailActivity.kt
+++ app/src/main/java/com/platzi/platzistore/DetailActivity.kt
@@ -2,12 +2,19 @@ package com.platzi.platzistore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.content_desc.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        intent?.extras?.let {
+            detail_title.text = it.getString("title")
+            detail_desc.text = it.getString("desc")
+            detail_price.text = "$ ${String.format("%.2f", it.getDouble("price"))}"
+        }
 
     }
 }
