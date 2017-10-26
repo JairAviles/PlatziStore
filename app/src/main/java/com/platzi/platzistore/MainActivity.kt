c8d019db666e6ea4862a1b7a90dcc3e2a30d136c
Jair Avilés <hi@jairaviles.mx>
2017-10-25 23:11:23 -0200

    add Content detail

--- app/src/main/java/com/platzi/platzistore/MainActivity.kt
+++ app/src/main/java/com/platzi/platzistore/MainActivity.kt
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         rc_landing.layoutManager = GridLayoutManager(this, 2)
 
-        val itemsShop = (0..20).map {
+        val itemsShop = (0..25).map {
             ItemLanding("Titulo $it", "Descripcion $it", 200.00 + it)
         }
 
